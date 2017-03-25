428206acfd130f97319385bab060408f5bd22a99
Antara Mahimkar <Antara Mahimkar>
2017-03-25 19:55:27 +0530

    Patient Dash

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,11 @@ router.get('/doctor/sch_appointments', function(req, res, next) {
 res.render('./doctor/sch_apt', { title: 'Schedule' });
 });
 
+router.get('/doctor/pen_appointments', function(req, res, next) {
+res.render('./doctor/pen_apt', { title: 'Pending' });
+});
+
+router.get('/patient', function(req, res, next) {
+res.render('./patient/index', { title: 'Home' });
+});
 module.exports = router;
