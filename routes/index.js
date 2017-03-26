f3ba3c0f3be03877fb823eb88c76944c1c3857ae
aditshinde <aditdineshshinde@gmail.com>
2017-03-26 08:04:51 +0530

    SMS feature added

--- routes/index.js
+++ routes/index.js
@@ -220,9 +220,21 @@ router.get('/doctor/accept', function (req, res, next) {
         else{
           appoint.findByDid(req.session.user._id,'PENDING',function (err,data){
                 if(err){
+
                   res.redirect('/doctor');
                 }
                 else{
+                  var twilio = require('twilio');
+ 
+                  // Find your account sid and auth token in your Twilio account Console.
+                  var client = twilio('AC633b1cda4b11ce6814b778251f817d44','17b8b974800bcd8d88551d40c2439474');
+                   
+                  // Send the text message.
+                  client.sendMessage({
+                    to: '+919768652121',
+                    from: '+12017304043',
+                    body: 'Your appointment has been accepted'
+                  });
                   res.render('./doctor/pen_apt', { title: 'Home', app : data });
                 }
           });
